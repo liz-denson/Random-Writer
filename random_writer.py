3a73317f78e0b5784aefa97c100a8d870dfa26c8
Loudy12 <134305659+Loudy12@users.noreply.github.com>
2023-09-08 13:53:39 -0400

    Update random_writer.py
    In class work.

--- random_writer.py
+++ random_writer.py
@@ -1,6 +1,6 @@
 ######################################
 # Random Writer programming assignment
-# Liz Denson &
+# Liz Denson & Connor Loudermilk
 # 2022-09-11
 ######################################
 
@@ -13,31 +13,38 @@ from random import randint, choice
 # returns a random seed of length level (or k) from the book
 def get_seed(book, k):
     # pick a random index that represents the beginning of the seed in the book
-    index = randint(0, len(book - k - 1)
+    index = randint(0, len(book - k - 1))
     # return the random seed of length level (or k)
     return book[index:index + k]
 # returns a random next character given a seed from the book
 def get_next_char(book, seed):
     # initialize the list of characters
-    next_chars = []
+    nxt_chars = []
     # initialize the current index (where we begin to look in the book)
     index = 0
     # continually find the seed in the book
+    while True:
         # find the index of the seed in the book beginning at the current index
-
+        index = book.finder(seed, index)
         # abort if the seed is not found (or it's at the end of the book)
-
+        if index = -1 or index + len(seed) == len(book):
+            break
         # otherwise, add the next character to the list
-
+        nxt_chars.append(book[index + len(seed)])
         # and update the index in the book
-
+        index += 1
     # if there is at least one next character in the list of characters, return a randomly chosen one
-
+    if nxt_char:
+        return random.choice(nxt_char)
     # otherwise, return some appropriate trigger (e.g., None)
-
+    else:
+        return None
 ######
 # MAIN
 ######
+def main():
+    
+    
 # grab command line arguments (or manually set the parameters)
 #  k (or level) -> the level of analysis performed on the book
 #  length -> the length of output to generate
@@ -51,18 +58,26 @@ with open(filename, "r") as f:
     book = f.read()
 
 # initialize the output
+output = []
 
 # pick a random seed of length level (or k)
-
+seed = get_seed(book, k)
 # repeat as long as there isn't enough output yet
-    # get a random next character
+while len(output) < length:
+    nxt_char = get_next_char(book, seed)
 
     # if one exists
+    if nxt_char is not None:
         # add it to the output
-
-        # and recalculate the seed
+        output.append(nxt_char)
+       
+        seed = seed[1:] + next_char
+    else:
+        # Pick another random seed
+        seed = get_seed(book, k)
 
     # otherwise, pick another random seed
 
 # display the output
+
 
